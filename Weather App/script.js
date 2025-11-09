b961a92b4ebb9a7e93c54e1af66b6609a5f197e6
ruskaruska <41624026+ruskaruska@users.noreply.github.com>
2025-11-09 11:37:31 +0400

    damateba

---
+++ Weather App/script.js
@@ -0,0 +1,9 @@
+function getWeather() {
+    const apiKey = 'YOUR-API-KEY';
+    const city = document.getElementById('city').value;
+
+    if (!city) {
+        alert('Please enter a city');
+        return;
+    }
+}
